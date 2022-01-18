538001c32c137ff896fa7e843b4fb7133bc167bb
Subhan Mahmood <sm01800@surrey.ac.uk>
2022-01-18 21:22:52 +0000

    link reordering implemented + link update on text change

--- pages/links/index.js
+++ pages/links/index.js
@@ -1,19 +1,22 @@
 import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import MainLayout from '../../lib/components/MainLayout'
 import AddLinkForm from 'lib/components/AddLinkForm'
+import LinkCard from 'lib/components/LinkCard'
+import Sortable from 'sortablejs';
+import { ReactSortable } from "react-sortablejs";
+
+
 
 
 export default function index() {
     const [links, setLinks] = useState([])
 
-    
-
     useEffect(() => {
         const fetchLinks = async () => {
             try {
                 const res = await axios.get('/api/link')
-                setLinks(res.data)
+                setLinks(res.data.sort((a, b) => a.order - b.order))
             } catch (err) {
                 console.log(err)
             }
@@ -21,27 +24,62 @@ export default function index() {
         fetchLinks()
     }, [])
 
-    const addLink = async () => {
+    const addLink = async (link) => {
         try {
-            const res = await axios.post('/api/link',
-                values)
-
-            resetForm()
+            link.order = links.length
+            const res = await axios.post('/api/link', link)
             setLinks([...links, res.data[0]])
         } catch (err) {
             console.log(err)
         }
     }
 
-    const deleteLink = async (linkId) => {
+    const updateLink = async (link, updateState) => {
         try {
-            const res = await axios.delete('/api/link', { data: { id: linkId } })
+            const res = await axios.put('/api/link', link)
             console.log(res)
-            setLinks(links.filter(l => l.id !== linkId))
+            if(updateState){
+                let updatedLinks = Object.assign(links)
+                const idx = updatedLinks.indexOf(updatedLinks.find(l => l.id === link.id))
+                updatedLinks[idx] = res.data[0];
+                setLinks([...updatedLinks])
+            }
         } catch (err) {
             console.log(err)
         }
     }
+
+    const deleteLink = async (link) => {
+        try {
+            const res = await axios.delete('/api/link', { data: { id: link.id } })
+            console.log(res)
+            setLinks(links.filter(l => l.id !== link.id))
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        let sorted = true;
+        for (let i = 0; i < links.length - 1; i++) {
+            if (links[i].order > links[i + 1].order) {
+                sorted = false;
+                break;
+            }
+        }
+
+        if (!sorted) {
+            let updatedLinks = Object.assign(links)
+            updatedLinks = updatedLinks.map((link, i) => {
+                link.order = i;
+                return link;
+            })
+            updatedLinks.forEach((link, i) => {
+                updateLink(link, false)
+            })
+            setLinks([...updatedLinks])
+        }
+    }, [links])
 
     return (
         <MainLayout>
@@ -49,15 +87,15 @@ export default function index() {
                 <h1 className="font-bold text-center text-6xl text-neutral-800">Links</h1>
                 <p className="text-neutral-600 text-center text-xl">View all my lnks</p>
                 <div className="max-w-[400px] mx-auto flex flex-col space-y-4 mt-4">
-                    {links.map((link, i) => {
-                        return (
-                            <div className="p-4 rounded-md border border-neutral-200 shadow-md flex justify-between" key={i}>
-                                <p>{link.name}</p>
-                                <span className="text-red-500 font-bold cursor-pointer" onClick={() => deleteLink(link.id)}>X</span>
-                            </div>
-                        )
-                    })}
-                    <AddLinkForm />
+                    <ReactSortable
+                        className="flex flex-col space-y-4"
+                        list={links}
+                        setList={setLinks}>
+                        {links.map((link, i) =>
+                            <LinkCard key={link.id} data={link} updateLink={updateLink} deleteLink={deleteLink} />)
+                        }
+                    </ReactSortable>
+                    <AddLinkForm onSubmit={addLink} />
                 </div>
             </div>
         </MainLayout>
