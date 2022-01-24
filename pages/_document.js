e7c9509d30330fb135c1995f638eec899955c3e1
Subhan Mahmood <sm01800@surrey.ac.uk>
2022-01-24 20:33:07 +0000

    udpated OG image

--- pages/_document.js
+++ pages/_document.js
@@ -20,8 +20,8 @@ class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&display=swap" rel="stylesheet" />
-                    <meta property="og:description" content="I'm a freelance developer and designer with a passion for creating great user experiences. I've been coding since I was 9, and in the last 10 years I've learnt a variety of technologies that enable me to craft high-quality, responsive digital experiences for my clients. Most recently, I've been helping small businesses and creators make the most of their customers and audiences, by providing them with great user experiences." />
-                    <meta property="og:image" content="/og-cover.jpg" />
+                    <meta property="og:description" content="I'm a freelance developer and designer with a passion for creating great user experiences. " />
+                    <meta property="og:image" content="https://images.prismic.io/subhan/a827ab94-a9ee-40d9-b7d1-c2e424a13d93_og-cover.jpg?auto=compress,format" />
                     <meta property="og:image:width" content="1200" />
                     <meta property="og:image:height" content="630" />
                 </Head>
